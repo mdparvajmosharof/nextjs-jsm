f4fd0fa3e25e1550913c7db79de2780cefc090a2
mdparvajmosharof <mdparvajmosharof17@gmail.com>
2024-12-16 05:26:39 +0600

    layout

---
+++ app/(dashboard)/dashboard/analytics/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      Hey ! you are in analityx, Ha Ha?
+      
+    </div>
+  )
+}
+
+export default page
---
+++ app/(dashboard)/dashboard/user/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      hey! whats uP its USer dashboard
+    </div>
+  )
+}
+
+export default page
